da7a29b69af875d40a563a74be14159377592ea4
J-Diggidy-Dawg <JasonDeJonge@outlook.com>
2021-07-18 17:33:10 +0100

    Added ColumnMapping and basic TableMapping

---
+++ DataChecker/DataChecker/Mapping/ColumnMapping.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DataCheckerProj.Mapping
+{
+    public class ColumnMapping
+    {
+        #region properties
+        public int MappingID { get; set; }
+        public string SourceColumnName { get; set; }
+        public string SourceColumnType { get; set; }
+        public string DestinationColumnName { get; set; }
+        public string DestinationColumnType { get; set; }
+        public bool IsIdentityColumn { get; set; }              // important. Used to identify mappings for a column that uniquelly identifies a data element (record)
+        public List<string> DecommissionedClasses { get; set; } // values for this column that constitute a class that should no longer be sampled
+        #endregion
+
+        public ColumnMapping(int id, string srcCol, string srcType, string dstCol, string dstType, bool isIdentity)
+        {
+            this.MappingID = id;
+            this.SourceColumnName = srcCol;
+            this.SourceColumnType = srcType;
+            this.DestinationColumnName = dstCol;
+            this.DestinationColumnType = dstType;
+            this.IsIdentityColumn = isIdentity;
+            this.DecommissionedClasses = new List<string>();
+        }
+    }
+}
---
+++ DataChecker/DataChecker/Mapping/TableMapping.cs
@@ -0,0 +1,65 @@
+﻿using DataCheckerProj.Importers;
+using System;
+using System.Collections.Generic;
+
+namespace DataCheckerProj.Mapping
+{
+    public class TableMapping
+    {
+        #region properties
+
+        public string SourceDatabaseName { get; set; }
+        public string SourceSchemaName { get; set; }
+        public string SourceTableName { get; set; }
+        public string DestinationDatabaseName { get; set; }
+        public string DestinationSchemaName { get; set; }
+        public string DestinationTableName { get; set; }
+        public List<ColumnMapping> MappedColumns;
+
+        #endregion
+
+        #region constructors
+        public TableMapping()
+        {
+            this.MappedColumns = new List<ColumnMapping>();
+        }
+
+        #endregion 
+
+        #region methods
+        public void LoadMappingFromSqlServerDatabase(SqlServerImporter mappingImporter, Dictionary<string, string> propertyToSourceAttributeMap)
+        {
+            /*
+             * Verify Params are not null 
+             */
+            if (mappingImporter == null)
+            {
+                throw new ArgumentException("Parameter mappingImporter cannot be null");
+            }
+            else if (propertyToSourceAttributeMap == null)
+            {
+                throw new ArgumentException("Parameter propertyToSourceAttributeMap cannot be null");
+            }
+
+            /* 
+             * Verify propertyToSourceAttributeMap contains required Property name mappings 
+             */
+
+
+            /* 
+             * Verify propertyToSourceAttributeMap contains source attribute names that match those returned by query used for provided SqlServerImporter
+             */
+
+            /*
+            * Load mappings from source 
+            */
+        }
+
+        public bool Validate()
+        {
+            return false;
+        }
+
+        #endregion
+    }
+}
